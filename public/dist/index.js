0834329c628fb97f0514815e5a99f47be79e1a0d
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-05-06 21:46:21 +0300

    i

--- public/dist/index.js
+++ public/dist/index.js
@@ -3,10 +3,14 @@ function handleCreatMyForm() {
     try {
         var newDiv = document.createElement("div");
         newDiv.classList.add(".newdiv");
-        var myForm = " <form onsubmit=\"handleRegister(e)\" class=\"registerForm\">\n        <h2 class=\"registerForm__h2\">Register Here</h2>\n        <label class=\"registerForm__label\" for=\"name\">Name:</label>\n        <input class=\"registerForm__input\"  type=\"text\" name=\"name\" placeholder=\"enter name\" required>\n        <label class=\"registerForm__label\"  for=\"email\">Email:</label>\n        <input class=\"registerForm__input\" type=\"email\" name=\"email\" placeholder=\"enter email\" required>\n        <label class=\"registerForm__label\"  for=\"password\">Password:</label>\n        <input class=\"registerForm__input\" type=\"text\" name=\"password\" placeholder=\"enter password\" required>\n        <button class=\"registerForm__btn\" type=\"submit\">Register</button>\n        </form>";
+        var myForm = "<form onsubmit=\"handleRegister(e)  class=\"registerForm\">\n        <h2 class=\"registerForm__h2\">Register Here</h2>\n        <label class=\"registerForm__label\" for=\"name\">Name:</label>\n        <input class=\"registerForm__input\"  type=\"text\" name=\"name\" placeholder=\"enter name\">\n        <label class=\"registerForm__label\"  for=\"email\">Email:</label>\n        <input class=\"registerForm__input\" type=\"email\" name=\"email\" placeholder=\"enter email\">\n        <label class=\"registerForm__label\"  for=\"password\">Password:</label>\n        <input class=\"registerForm__input\" type=\"password\" name=\"password\" placeholder=\"enter password\">\n        <button class=\"registerForm__btn\" type=\"submit\">Register</button>\n        </form>";
         var container = document.querySelector(".container");
         if (!container)
             throw new Error("couldent find main container");
+        if (!myForm)
+            throw new Error("couldent find myForm");
+        if (!newDiv)
+            throw new Error("couldent find newDiv");
         container.appendChild(newDiv);
         newDiv.innerHTML = myForm;
     }
@@ -16,6 +20,9 @@ function handleCreatMyForm() {
 }
 // the register function
 function handleRegister(e) {
+    e.preventDefault();
+    console.log(e);
+    console.log("form register working");
 }
 // the sign in function
 function handleSignIn() {
--- public/index.ts
+++ public/index.ts
@@ -1,22 +1,27 @@
 // creat the form for register + option to change to sign in only
 function handleCreatMyForm(){
     try {
-        const newDiv = document.createElement("div")
+       const newDiv = document.createElement("div")
         newDiv.classList.add(".newdiv")
-        const myForm = ` <form onsubmit="handleRegister(e)" class="registerForm">
+        const myForm = `<form onsubmit="handleRegister(e)  class="registerForm">
         <h2 class="registerForm__h2">Register Here</h2>
         <label class="registerForm__label" for="name">Name:</label>
-        <input class="registerForm__input"  type="text" name="name" placeholder="enter name" required>
+        <input class="registerForm__input"  type="text" name="name" placeholder="enter name">
         <label class="registerForm__label"  for="email">Email:</label>
-        <input class="registerForm__input" type="email" name="email" placeholder="enter email" required>
+        <input class="registerForm__input" type="email" name="email" placeholder="enter email">
         <label class="registerForm__label"  for="password">Password:</label>
-        <input class="registerForm__input" type="text" name="password" placeholder="enter password" required>
+        <input class="registerForm__input" type="password" name="password" placeholder="enter password">
         <button class="registerForm__btn" type="submit">Register</button>
         </form>`
         const container = document.querySelector(".container")as HTMLDivElement | null;
        if(!container)throw new Error("couldent find main container");
+       if(!myForm)throw new Error("couldent find myForm");
+       if(!newDiv)throw new Error("couldent find newDiv");
         container.appendChild(newDiv)
         newDiv.innerHTML = myForm
+
+
+
     } catch (error) {
       console.error(error)  
     }
@@ -26,8 +31,10 @@ function handleCreatMyForm(){
 
 
 // the register function
-function handleRegister(e){
-
+function handleRegister(e: any){
+  e.preventDefault()
+  console.log(e)
+console.log("form register working")
 
 
 
