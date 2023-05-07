1738756cd9b47c28c78fd93f1c1bde7a02964265
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-05-07 10:57:11 +0300

    fix form

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,29 +1,32 @@
 // creat the form for register + option to change to sign in only
 function handleCreatMyForm() {
     try {
-        var newDiv = document.createElement("div");
-        newDiv.classList.add(".newdiv");
-        var myForm = "<form onsubmit= \"handleRegister(event)\"  class=\"registerForm\">\n        <h2 class=\"registerForm__h2\">Register Here</h2>\n        <label class=\"registerForm__label\" for=\"name\">Name:</label>\n        <input class=\"registerForm__input\"  type=\"text\" name=\"name\" placeholder=\"enter name\">\n        <label class=\"registerForm__label\"  for=\"email\">Email:</label>\n        <input class=\"registerForm__input\" type=\"email\" name=\"email\" placeholder=\"enter email\">\n        <label class=\"registerForm__label\"  for=\"password\">Password:</label>\n        <input class=\"registerForm__input\" type=\"password\" name=\"password\" placeholder=\"enter password\">\n        <button class=\"registerForm__btn\" type=\"submit\">Register</button>\n        </form>";
+        var myForm = "<form onsubmit= \"handleRegister(event)\"  class=\"registerForm\">\n        <h2 class=\"registerForm__h2\">Register Here</h2>\n        <label class=\"registerForm__label\" for=\"name\">Name:</label>\n        <input class=\"registerForm__input\"  type=\"text\" name=\"name\" placeholder=\"enter name\">\n        <label class=\"registerForm__label\"  for=\"email\">Email:</label>\n        <input class=\"registerForm__input\" type=\"email\" name=\"email\" placeholder=\"enter email\">\n        <label class=\"registerForm__label\"  for=\"password\">Password:</label>\n        <input class=\"registerForm__input\" type=\"password\" name=\"password\" placeholder=\"enter password\">\n        <button class=\"registerForm__btn\" type=\"submit\">Register</button>\n        </form>\n        <div class=\"moveToSignIn\"><button class=\"moveToSignIn__btn\" onclick=\"handleSignIn()\">sign-in</button></div>\n        ";
         var container = document.querySelector(".container");
         if (!container)
             throw new Error("couldent find main container");
-        if (!myForm)
-            throw new Error("couldent find myForm");
-        if (!newDiv)
-            throw new Error("couldent find newDiv");
-        container.appendChild(newDiv);
-        newDiv.innerHTML = myForm;
+        container.innerHTML = myForm;
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+// the sign in function
+function handleSignIn() {
+    try {
+        console.log("sign in function");
+        var container = document.querySelector(".container");
+        if (!container)
+            throw new Error("couldent find main container");
+        var signIn = "<form onsubmit= \"handleRegister(event)\"  class=\"registerForm\">\n    <h2 class=\"registerForm__h2\">Sign In Here</h2>\n    <label class=\"registerForm__label\" for=\"name\">Name:</label>\n    <input class=\"registerForm__input\"  type=\"text\" name=\"name\" placeholder=\"enter name\">\n    <label class=\"registerForm__label\"  for=\"password\">Password:</label>\n    <input class=\"registerForm__input\" type=\"password\" name=\"password\" placeholder=\"enter password\">\n    <button class=\"registerForm__btn\" type=\"submit\">sign-in</button>\n    </form>\n    <div class=\"moveToRegister\"><button class=\"moveToRegister__btn\" onclick=\"handleCreatMyForm()\">go register</button></div>\n    ";
+        container.innerHTML = signIn;
     }
     catch (error) {
         console.error(error);
     }
 }
 // the register function
-function handleRegister(e) {
-    e.preventDefault();
-    console.log(e);
-    console.log("form register working");
-}
-// the sign in function
-function handleSignIn() {
+function handleRegister(event) {
+    event.preventDefault();
+    console.log("itsik");
 }
--- public/index.ts
+++ public/index.ts
@@ -1,8 +1,6 @@
 // creat the form for register + option to change to sign in only
 function handleCreatMyForm(){
     try {
-       const newDiv = document.createElement("div")
-        newDiv.classList.add(".newdiv")
         const myForm = `<form onsubmit= "handleRegister(event)"  class="registerForm">
         <h2 class="registerForm__h2">Register Here</h2>
         <label class="registerForm__label" for="name">Name:</label>
@@ -12,36 +10,48 @@ function handleCreatMyForm(){
         <label class="registerForm__label"  for="password">Password:</label>
         <input class="registerForm__input" type="password" name="password" placeholder="enter password">
         <button class="registerForm__btn" type="submit">Register</button>
-        </form>`
-        const container = document.querySelector(".container")as HTMLDivElement | null;
+        </form>
+        <div class="moveToSignIn"><button class="moveToSignIn__btn" onclick="handleSignIn()">sign-in</button></div>
+        `
+       const container = document.querySelector(".container")as HTMLDivElement | null;
        if(!container)throw new Error("couldent find main container");
-       if(!myForm)throw new Error("couldent find myForm");
-       if(!newDiv)throw new Error("couldent find newDiv");
-        container.appendChild(newDiv)
-        newDiv.innerHTML = myForm
-
-
-
-    } catch (error) {
+      container.innerHTML = myForm
+        
+      } catch (error) {
       console.error(error)  
     }
    
+}
+
+// the sign in function
+function handleSignIn(){
+  try {
+    console.log("sign in function")
+    const container = document.querySelector(".container")as HTMLDivElement | null;
+    if(!container)throw new Error("couldent find main container");
+     const signIn = `<form onsubmit= "handleRegister(event)"  class="registerForm">
+    <h2 class="registerForm__h2">Sign In Here</h2>
+    <label class="registerForm__label" for="name">Name:</label>
+    <input class="registerForm__input"  type="text" name="name" placeholder="enter name">
+    <label class="registerForm__label"  for="password">Password:</label>
+    <input class="registerForm__input" type="password" name="password" placeholder="enter password">
+    <button class="registerForm__btn" type="submit">sign-in</button>
+    </form>
+    <div class="moveToRegister"><button class="moveToRegister__btn" onclick="handleCreatMyForm()">go register</button></div>
+    `
+      container.innerHTML = signIn
+    } catch (error) {
+    console.error(error)
+  }
+  
+  
 }
 
 
 // the register function
 function handleRegister(event: any){
   event.preventDefault()
-  console.log(event)
-console.log("form register working")
+ console.log("itsik")
 
 
-
-}
-
-
-
-// the sign in function
-function handleSignIn(){
-    
 }
