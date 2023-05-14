05ca55c1cedb3e0f728cb3d1b3413a6f62f55fdc
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-05-14 14:00:30 +0300

    i

--- public/index.ts
+++ public/index.ts
@@ -1,33 +1,59 @@
 // creat the form for register + option to change to sign in only
-// function handleCreatMyForm(){
-
-
-
-
-
-
-// }
+function handleCreatMyForm(){
+  
+  const html = `
+  
+  <div class="wrapper">
+  <div class="wrapper__info"></div>
+<div class="container">
+  <div class="container__form">
+      <div class="container__form__SignIn">
+      <button class="container__form__SignIn__btnsign" id="btnsign" onclick="handleSignIn()">sign-in</button></div>
+      <form onsubmit= "handleRegister(event)" class="registerForm"> 
+      <h2 class="registerForm__h2">Register Here</h2>
+      <label class="registerForm__label" for="name">Name:</label>
+      <input class="registerForm__input"  type="text" name="name" placeholder="enter name">
+      <label class="registerForm__label"  for="email">Email:</label>
+     <input class="registerForm__input" type="email" name="email" placeholder="enter email">
+      <label class="registerForm__label"  for="password">Password:</label>
+          <input class="registerForm__input" type="password" name="password" placeholder="enter password">
+          <br>
+              <button class="registerForm__btn" type="submit">Register</button>
+              </form>
+  </div>
+            <div class="container__forminfo">
+              <h1 class="container__forminfo__h1">Tinder<span>FY</span></h1>
+              <h2 class="container__forminfo__h2">still Alone, no worry here you can find what you looking for.</h2>
+              <h3 class="container__forminfo__h3">consider our sub for 100% chance, dont stay alone at night</h3>
+            </div>
+</div>
+</div>
+    `
+    const body = document.querySelector("body")as HTMLBodyElement;
+    body.innerHTML = html
+}
 
 // the sign in function
 function handleSignIn(){
-  try {
-      const container = document.querySelector(".container")as HTMLDivElement | null;
-    if(!container)throw new Error("couldent find main container");
-     const signIn = `<form onsubmit= "handleLogin(event)" class="registerForm">
-    <h2 class="registerForm__h2">Sign In Here</h2>
-    <label class="registerForm__label" for="name">Name:</label>
-    <input class="registerForm__input"  type="text" name="name" placeholder="enter name">
-    <label class="registerForm__label"  for="password">Password:</label>
-    <input class="registerForm__input" type="password" name="password" placeholder="enter password">
-    <button class="registerForm__btn" type="submit">sign-in</button>
-    </form>
-    <div class="moveToRegister"><button class="moveToRegister__btn" onclick="handleCreatMyForm()">go register</button></div>
-    `
-      container.innerHTML = signIn
-    } catch (error) {
-    console.error(error)
-  }
-  
+ 
+  const html = `
+    
+  <div class="wrapper">
+  <div class="wrapper__info"></div>
+<div class="container">
+  <div class="container__form">
+      <div class="container__form__SignIn">
+      <button class="container__form__SignIn__btnsign" id="btnsign" onclick="handleSignIn()">sign-in</button></div>
+      <form onsubmit= "handleRegister(event)" class="registerForm"> 
+      <h2 class="registerForm__h2">Register Here</h2>
+      <label class="registerForm__label" for="name">Name:</label>
+      <input class="registerForm__input"  type="text" name="name" placeholder="enter name">
+       <label class="registerForm__label"  for="password">Password:</label>
+          <input class="registerForm__input" type="password" name="password" placeholder="enter password">
+          <br>
+              <button class="registerForm__btn" type="submit">Register</button>
+              </form>
+  `
   
 }
 
