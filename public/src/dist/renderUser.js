3cd5c3aca9d7d7bd04209a89f4bf7a6d011cf793
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-05-15 19:45:06 +0300

    i

--- public/src/dist/renderUser.js
+++ public/src/dist/renderUser.js
@@ -1,2 +1,20 @@
-// function renderUserPage(){
-// }
+function renderUserPage() {
+    var h2User = document.querySelector("h2User");
+    try {
+        fetch("/newProfile", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify()
+        })
+            .then(function (res) { return res.json(); })
+            .then(function (data) {
+            console.log(data);
+        });
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
--- public/src/renderUser.ts
+++ public/src/renderUser.ts
@@ -1,5 +1,29 @@
 
-// function renderUserPage(){
+function renderUserPage(){
+const h2User = document.querySelector("h2User")as HTMLHeadElement|null;
+
+try {
+    
+    fetch("/newProfile", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        
+         })
+       
+    } catch (error) {
+      console.error(error)
+    }
+      
+  }
 
 
-// }
+
+  
