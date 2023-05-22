4736001faba01340fd7a631f0ba44c3f0e37aff6
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-05-22 19:18:31 +0300

    Revert "i"
    This reverts commit 54869451a58479cf8d6e0c1983f24799b9cbd86c.

--- API/users/usersModel.ts
+++ API/users/usersModel.ts
@@ -12,7 +12,6 @@ export const UserSchema = new Schema ({
     // role: {type:String, enum:["admin", "user"],default:"user"},
     // subscribed:{type:Boolean, default:false, select:false},
 
-<<<<<<< HEAD
     // age: {require:true, type:Number,},
     // gender: {require:true, type:String,},
     // location: {require:true, type:String,},
@@ -25,20 +24,6 @@ export const UserSchema = new Schema ({
     // relationship: {require:true, type:String,},
     // info: {require:true, type:String,},
     // religious: {require:true, type:String,}
-=======
-    age: {require:true, type:Number,},
-    gender: {require:true, type:String,},
-    location: {require:true, type:String,},
-    height: {require:true, type:Number,},
-    bodyType: {require:true, type:String,},
-    kids: {require:true, type:String,},
-    smoking: {require:true, type:String,},
-    education: {require:true, type:String,},
-    job: {require:true, type:String,},
-    relationship: {require:true, type:String,},
-    info: {require:true, type:String,},
-    religious: {require:true, type:String,},
->>>>>>> parent of 3225e94 (html form + model of preffenrance)
    })
        
 
